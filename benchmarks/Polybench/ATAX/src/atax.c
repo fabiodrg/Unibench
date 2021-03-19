3cb18dc1b3fe701ba5d5c5dd6f19dee6d76599e9
Fabio Gaspar <fabiodanielreis@hotmail.com>
2021-03-19 22:25:42 +0000

    Adapt Polybench/ATAX

--- benchmarks/Polybench/ATAX/src/atax.c
+++ benchmarks/Polybench/ATAX/src/atax.c
@@ -22,15 +22,11 @@
 #include "BenchmarksUtil.h"
 
 // define the error threshold for the results "not matching"
-#define PERCENT_DIFF_ERROR_THRESHOLD 0.5
+#define ERROR_THRESHOLD 0.5
 
 /* Problem size. */
-#ifdef RUN_TEST
-#define SIZE 1100
-#elif RUN_BENCHMARK
-#define SIZE 9600
-#else
-#define SIZE 1000
+#ifndef SIZE
+#define SIZE 1024
 #endif
 
 #define NX SIZE
@@ -54,25 +50,25 @@ void init_array(DATA_TYPE *x, DATA_TYPE *A) {
   }
 }
 
-int compareResults(DATA_TYPE *z, DATA_TYPE *z_outputFromGpu) {
+int compareResults(DATA_TYPE *z, DATA_TYPE *z_OMP) {
   int i, fail;
   fail = 0;
 
   for (i = 0; i < NY; i++) {
-    if (percentDiff(z[i], z_outputFromGpu[i]) > PERCENT_DIFF_ERROR_THRESHOLD) {
+    if (percentDiff(z[i], z_OMP[i]) > ERROR_THRESHOLD) {
       fail++;
     }
   }
 
   // print results
-  printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f "
-         "Percent: %d\n",
-         PERCENT_DIFF_ERROR_THRESHOLD, fail);
+  // printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f "
+  //        "Percent: %d\n",
+  //        ERROR_THRESHOLD, fail);
 
   return fail;
 }
 
-void atax_cpu(DATA_TYPE *A, DATA_TYPE *x, DATA_TYPE *y, DATA_TYPE *tmp) {
+void atax(DATA_TYPE *A, DATA_TYPE *x, DATA_TYPE *y, DATA_TYPE *tmp) {
   int i, j;
 
   for (i = 0; i < NY; i++) {
@@ -98,7 +94,7 @@ void atax_OMP(DATA_TYPE *A, DATA_TYPE *x, DATA_TYPE *y, DATA_TYPE *tmp) {
     y[i] = 0;
   }
 
-  #pragma omp target teams map(to : A[ : NX *NY], x[ : NY]) map(tofrom : tmp[ : NX], y[ : NY]) device(DEVICE_ID)
+  #pragma omp target teams map(to : A[ : NX *NY], x[ : NY]) map(tofrom : tmp[ : NX], y[ : NY]) device(OMP_DEVICE_ID)
   {
     #pragma omp distribute parallel for
     for (int i = 0; i < NX; i++) {
@@ -125,37 +121,38 @@ int main(int argc, char **argv) {
   DATA_TYPE *A;
   DATA_TYPE *x;
   DATA_TYPE *y;
-  DATA_TYPE *y_outputFromGpu;
+  DATA_TYPE *y_OMP;
   DATA_TYPE *tmp;
 
   A = (DATA_TYPE *)malloc(NX * NY * sizeof(DATA_TYPE));
   x = (DATA_TYPE *)malloc(NY * sizeof(DATA_TYPE));
   y = (DATA_TYPE *)malloc(NY * sizeof(DATA_TYPE));
-  y_outputFromGpu = (DATA_TYPE *)malloc(NY * sizeof(DATA_TYPE));
+  y_OMP = (DATA_TYPE *)malloc(NY * sizeof(DATA_TYPE));
   tmp = (DATA_TYPE *)malloc(NX * sizeof(DATA_TYPE));
 
   fprintf(stdout, "<< Matrix Transpose and Vector Multiplication >>\n");
 
   init_array(x, A);
 
-  t_start = rtclock();
-  atax_OMP(A, x, y_outputFromGpu, tmp);
-  t_end = rtclock();
-  fprintf(stdout, "GPU Runtime: %0.6lfs\n", t_end - t_start);
+// run OMP on GPU or CPU if enabled
+#if defined(RUN_OMP_GPU) || defined(RUN_OMP_CPU)
+  BENCHMARK_OMP(atax_OMP(A, x, y_OMP, tmp));
+#endif
+
+// run sequential version if enabled
+#ifdef RUN_CPU_SEQ
+  BENCHMARK_CPU(atax(A, x, y, tmp));
+#endif
 
 #ifdef RUN_TEST
-  t_start = rtclock();
-  atax_cpu(A, x, y, tmp);
-  t_end = rtclock();
-  fprintf(stdout, "CPU Runtime: %0.6lfs\n", t_end - t_start);
-
-  fail = compareResults(y, y_outputFromGpu);
+  fail = compareResults(y, y_OMP);
+  printf("Errors on OMP (threshold %4.2lf): %d\n", ERROR_THRESHOLD, fail);
 #endif
 
   free(A);
   free(x);
   free(y);
-  free(y_outputFromGpu);
+  free(y_OMP);
   free(tmp);
 
   return fail;
