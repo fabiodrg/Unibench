6890474c6df2f23ab4277ef4572675ffd6ad6aee
Fabio Gaspar <fabiodanielreis@hotmail.com>
2021-06-24 15:04:45 +0100

    feat: Support for Polybench/FDTD-2D

--- benchmarks/Polybench/FDTD-2D/src/fdtd2d.c
+++ benchmarks/Polybench/FDTD-2D/src/fdtd2d.c
@@ -15,23 +15,12 @@
 #include <stdlib.h>
 #include <sys/time.h>
 #include <unistd.h>
+#include <string.h>
 #ifdef _OPENMP
 #include <omp.h>
 #endif
 
 #include "BenchmarksUtil.h"
-
-// define the error threshold for the results "not matching"
-#define PERCENT_DIFF_ERROR_THRESHOLD 10.05
-
-/* Problem size. */
-#ifdef RUN_TEST
-#define SIZE 1100
-#elif RUN_BENCHMARK
-#define SIZE 9600
-#else
-#define SIZE 1000
-#endif
 
 #define tmax 500
 #define NX SIZE
@@ -73,17 +62,11 @@ int compareResults(DATA_TYPE *hz1, DATA_TYPE *hz2) {
 
   for (i = 0; i < NX; i++) {
     for (j = 0; j < NY; j++) {
-      if (percentDiff(hz1[i * NY + j], hz2[i * NY + j]) >
-          PERCENT_DIFF_ERROR_THRESHOLD) {
+      if (percentDiff(hz1[i * NY + j], hz2[i * NY + j]) > ERROR_THRESHOLD) {
         fail++;
       }
     }
   }
-
-  // Print results
-  printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f "
-         "Percent: %d\n",
-         PERCENT_DIFF_ERROR_THRESHOLD, fail);
 
   return fail;
 }
@@ -125,15 +108,15 @@ void runFdtd_OMP(DATA_TYPE *_fict_, DATA_TYPE *ex, DATA_TYPE *ey,
                  DATA_TYPE *hz) {
   int t, i, j;
   
-  #pragma omp target data  map(to : _fict_[ : tmax], ex[ : (NX *(NY + 1))], ey[ : ((NX + 1) * NY)]) map(tofrom : hz[ : (NX *(NY + 1))]) device(DEVICE_ID)
+  #pragma omp target data  map(to : _fict_[ : tmax], ex[ : (NX *(NY + 1))], ey[ : ((NX + 1) * NY)]) map(tofrom : hz[ : (NX *(NY + 1))]) device(OMP_DEVICE_ID)
   {
     for (t = 0; t < tmax; t++) {
-      #pragma omp target teams distribute parallel for device(DEVICE_ID)
+      #pragma omp target teams distribute parallel for device(OMP_DEVICE_ID)
       for (j = 0; j < NY; j++) {
         ey[0 * NY + j] = _fict_[t];
       }
 
-      #pragma omp target teams distribute parallel for collapse(2) device(DEVICE_ID)
+      #pragma omp target teams distribute parallel for collapse(2) device(OMP_DEVICE_ID)
       for (i = 1; i < NX; i++) {
         for (j = 0; j < NY; j++) {
           ey[i * NY + j] =
@@ -141,7 +124,7 @@ void runFdtd_OMP(DATA_TYPE *_fict_, DATA_TYPE *ex, DATA_TYPE *ey,
         }
       }
 
-      #pragma omp target teams distribute parallel for collapse(2) device(DEVICE_ID)
+      #pragma omp target teams distribute parallel for collapse(2) device(OMP_DEVICE_ID)
       for (i = 0; i < NX; i++) {
         for (j = 1; j < NY; j++) {
           ex[i * (NY + 1) + j] = ex[i * (NY + 1) + j] -
@@ -149,7 +132,7 @@ void runFdtd_OMP(DATA_TYPE *_fict_, DATA_TYPE *ex, DATA_TYPE *ey,
         }
       }
       
-      #pragma omp target teams distribute parallel for collapse(2) device(DEVICE_ID)
+      #pragma omp target teams distribute parallel for collapse(2) device(OMP_DEVICE_ID)
       for (i = 0; i < NX; i++) {
         for (j = 0; j < NY; j++) {
           hz[i * NY + j] =
@@ -163,42 +146,51 @@ void runFdtd_OMP(DATA_TYPE *_fict_, DATA_TYPE *ex, DATA_TYPE *ey,
 }
 
 int main() {
-  double t_start, t_end;
-  int fail = 0;
-
-  DATA_TYPE *_fict_;
-  DATA_TYPE *ex;
-  DATA_TYPE *ey;
-  DATA_TYPE *hz;
-  DATA_TYPE *hz_outputFromGpu;
-
-  _fict_ = (DATA_TYPE *)malloc(tmax * sizeof(DATA_TYPE));
-  ex = (DATA_TYPE *)malloc(NX * (NY + 1) * sizeof(DATA_TYPE));
-  ey = (DATA_TYPE *)malloc((NX + 1) * NY * sizeof(DATA_TYPE));
-  hz = (DATA_TYPE *)malloc(NX * NY * sizeof(DATA_TYPE));
-  hz_outputFromGpu = (DATA_TYPE *)malloc(NX * NY * sizeof(DATA_TYPE));
-
   fprintf(stdout, "<< 2-D Finite Different Time Domain Kernel >>\n");
 
-  init_arrays(_fict_, ex, ey, hz);
-  init_array_hz(hz_outputFromGpu);
+  // declare arrays and allocate memory
+  DATA_TYPE *_fict_ = (DATA_TYPE *)calloc(tmax, sizeof(DATA_TYPE));
+  DATA_TYPE *ex = (DATA_TYPE *)calloc(NX * (NY + 1), sizeof(DATA_TYPE));
+  DATA_TYPE *ey = (DATA_TYPE *)calloc((NX + 1) * NY, sizeof(DATA_TYPE));
+  DATA_TYPE *hz = NULL;
+  DATA_TYPE *hz_outputFromGpu = NULL;
 
-  t_start = rtclock();
-  runFdtd_OMP(_fict_, ex, ey, hz_outputFromGpu);
-  t_end = rtclock();
-
-  fprintf(stdout, "GPU Runtime: %0.6lfs\n", t_end - t_start);
-
-#ifdef RUN_TEST
-  t_start = rtclock();
-  runFdtd(_fict_, ex, ey, hz);
-  t_end = rtclock();
-
-  fprintf(stdout, "CPU Runtime: %0.6lfs\n", t_end - t_start);
-
-  fail = compareResults(hz, hz_outputFromGpu);
+// run OMP on GPU or CPU if enabled
+#if defined(RUN_OMP_GPU) || defined(RUN_OMP_CPU)
+  // allocate
+  hz_outputFromGpu = (DATA_TYPE *) malloc(NX * NY * sizeof(DATA_TYPE));
+  // init arrays
+  init_arrays(_fict_, ex, ey, hz_outputFromGpu);
+  // benchmark
+  BENCHMARK_OMP(runFdtd_OMP(_fict_, ex, ey, hz_outputFromGpu));
 #endif
 
+// run sequential version if enabled
+#ifdef RUN_CPU_SEQ
+  // reset memory for common/shared arrays
+  // NOTE: it seems the init_arrays does not overwrite all memory positions
+  // on ex and ey. In turn, that seems to affect the final output and generate
+  // errors. Since I dont know the algorithm, this is a quick fix that resets
+  // the arrays with 0s, the then initializes everything again
+  memset(_fict_, 0, tmax * sizeof(DATA_TYPE));
+  memset(ex, 0, NX * (NY + 1) * sizeof(DATA_TYPE));
+  memset(ey, 0, (NX + 1) * NY * sizeof(DATA_TYPE));
+  // allocate
+  hz = (DATA_TYPE *) malloc(NX * NY * sizeof(DATA_TYPE));
+  // init arrays
+  init_arrays(_fict_, ex, ey, hz);
+  // benchmark
+  BENCHMARK_CPU(runFdtd(_fict_, ex, ey, hz));
+#endif
+
+  // if TEST is enabled, then compare OMP results against sequential mode
+  int fail = 0;
+#ifdef RUN_TEST
+  fail = compareResults(hz, hz_outputFromGpu);
+  printf("Errors on OMP (threshold %4.2lf): %d\n", ERROR_THRESHOLD, fail);
+#endif
+
+  // release memory
   free(_fict_);
   free(ex);
   free(ey);
