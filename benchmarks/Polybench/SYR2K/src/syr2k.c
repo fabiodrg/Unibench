4bfd5fc09ab454a2b967386d4bb0d097b8afd97b
Fabio Gaspar <fabiodanielreis@hotmail.com>
2021-06-25 15:51:27 +0100

    feat: Add support for SYR2K

--- benchmarks/Polybench/SYR2K/src/syr2k.c
+++ benchmarks/Polybench/SYR2K/src/syr2k.c
@@ -21,18 +21,6 @@
 
 #include "BenchmarksUtil.h"
 
-// define the error threshold for the results "not matching"
-#define PERCENT_DIFF_ERROR_THRESHOLD 0.10
-
-/* Problem size */
-#ifdef RUN_TEST
-#define SIZE 1100
-#elif RUN_BENCHMARK
-#define SIZE 9600
-#else
-#define SIZE 1000
-#endif
-
 #define N SIZE
 #define M SIZE
 
@@ -44,17 +32,23 @@
 /* Can switch DATA_TYPE between float and double */
 typedef float DATA_TYPE;
 
-void init_arrays(DATA_TYPE *A, DATA_TYPE *B, DATA_TYPE *C) {
+void init_arrays(DATA_TYPE *A, DATA_TYPE *B) {
+  int i, j;
+
+  for (i = 0; i < N; i++) {
+    for (j = 0; j < M; j++) {
+      A[i * N + j] = ((DATA_TYPE)i * j) / N;
+      B[i * N + j] = ((DATA_TYPE)i * j + 1) / N;
+    }
+  }
+}
+
+void init_C_array(DATA_TYPE *C) {
   int i, j;
 
   for (i = 0; i < N; i++) {
     for (j = 0; j < N; j++) {
       C[i * N + j] = ((DATA_TYPE)i * j + 2) / N;
-    }
-
-    for (j = 0; j < M; j++) {
-      A[i * N + j] = ((DATA_TYPE)i * j) / N;
-      B[i * N + j] = ((DATA_TYPE)i * j + 1) / N;
     }
   }
 }
@@ -78,19 +72,17 @@ void syr2k(DATA_TYPE *A, DATA_TYPE *B, DATA_TYPE *C) {
   }
 }
 
-void syr2k_OMP(DATA_TYPE *A, DATA_TYPE *B, DATA_TYPE *C, DATA_TYPE *Cinit) {
-
+void syr2k_OMP(DATA_TYPE *A, DATA_TYPE *B, DATA_TYPE *C) {
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      Cinit[i * N + j] *= BETA;
+      C[i * N + j] *= BETA;
     }
   }
 
-  #pragma omp target teams map(to : A[ : N *M], B[ : N *M], Cinit[ : N *N]) map(from : C[ : N *N]) device(DEVICE_ID)
+  #pragma omp target teams map(to : A[ : N *M], B[ : N *M]) map(tofrom : C[ : N *N]) device(OMP_DEVICE_ID)
   #pragma omp distribute parallel for collapse(2)
   for (int i = 0; i < N; i++) {
     for (int j = 0; j < N; j++) {
-      C[i * N + j] = Cinit[i * N + j];
       for (int k = 0; k < M; k++) {
         C[i * N + j] += ALPHA * A[i * M + k] * B[j * M + k];
         C[i * N + j] += ALPHA * B[i * M + k] * A[j * M + k];
@@ -99,67 +91,58 @@ void syr2k_OMP(DATA_TYPE *A, DATA_TYPE *B, DATA_TYPE *C, DATA_TYPE *Cinit) {
   }
 }
 
-int compareResults(DATA_TYPE *C, DATA_TYPE *C_Gpu) {
+int compareResults(DATA_TYPE *C, DATA_TYPE *C_OMP) {
   int i, j, fail;
   fail = 0;
 
-  // Compare C with D
   for (i = 0; i < N; i++) {
     for (j = 0; j < N; j++) {
-      if (percentDiff(C[i * N + j], C_Gpu[i * N + j]) >
-          PERCENT_DIFF_ERROR_THRESHOLD) {
+      if (percentDiff(C[i * N + j], C_OMP[i * N + j]) > ERROR_THRESHOLD) {
         fail++;
       }
     }
   }
 
-  // print results
-  printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f "
-         "Percent: %d\n",
-         PERCENT_DIFF_ERROR_THRESHOLD, fail);
   return fail;
 }
 
 int main() {
-  double t_start, t_end;
-  int fail = 0;
-
-  DATA_TYPE *A;
-  DATA_TYPE *B;
-  DATA_TYPE *C;
-  DATA_TYPE *Cinit;
-  DATA_TYPE *C_Gpu;
-
-  A = (DATA_TYPE *)malloc(N * M * sizeof(DATA_TYPE));
-  B = (DATA_TYPE *)malloc(N * M * sizeof(DATA_TYPE));
-  C = (DATA_TYPE *)malloc(N * M * sizeof(DATA_TYPE));
-  Cinit = (DATA_TYPE *)malloc(N * M * sizeof(DATA_TYPE));
-  C_Gpu = (DATA_TYPE *)calloc(N * M, sizeof(DATA_TYPE));
-
   fprintf(stdout, "<< Symmetric rank-2k operations >>\n");
+  
+  // Declare arrays and allocate memory for common arrays
+  DATA_TYPE *A = (DATA_TYPE *)malloc(N * M * sizeof(DATA_TYPE));
+  DATA_TYPE *B = (DATA_TYPE *)malloc(N * M * sizeof(DATA_TYPE));
+  DATA_TYPE *C = NULL;
+  DATA_TYPE *C_OMP = NULL;
 
-  init_arrays(A, B, Cinit);
+  // Initialize common arrays
+  init_arrays(A, B);
 
-  t_start = rtclock();
-  syr2k_OMP(A, B, C_Gpu, Cinit);
-  t_end = rtclock();
-  fprintf(stdout, "GPU Runtime: %0.6lfs\n", t_end - t_start);
+// run OMP on GPU or CPU if enabled
+#if defined(RUN_OMP_GPU) || defined(RUN_OMP_CPU)
+  C_OMP = (DATA_TYPE *) calloc(N * M, sizeof(DATA_TYPE));
+  init_C_array(C_OMP);
+  BENCHMARK_OMP(syr2k_OMP(A, B, C_OMP));
+#endif
 
+// run sequential version if enabled
+#ifdef RUN_CPU_SEQ
+  C = (DATA_TYPE *) calloc(N * M, sizeof(DATA_TYPE));
+  init_C_array(C);
+  BENCHMARK_CPU(syr2k(A, B, C));
+#endif
+
+  // if TEST is enabled, then compare OMP results against sequential mode
+  int fail = 0;
 #ifdef RUN_TEST
-  init_arrays(A, B, C);
-
-  t_start = rtclock();
-  syr2k(A, B, C);
-  t_end = rtclock();
-  fprintf(stdout, "CPU Runtime: %0.6lfs\n", t_end - t_start);
-
-  fail = compareResults(C, C_Gpu);
+  fail = compareResults(C, C_OMP);
+  printf("Errors on OMP (threshold %4.2lf): %d\n", ERROR_THRESHOLD, fail);
 #endif
 
   free(A);
   free(B);
   free(C);
-  free(C_Gpu);
+  free(C_OMP);
 
   return fail;
 }
