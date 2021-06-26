0754359c239d790b6c58212f953546cb50ae73f7
Fabio Gaspar <fabiodanielreis@hotmail.com>
2021-06-26 23:06:22 +0100

    feat: Support Polybench/CORR

--- benchmarks/Polybench/CORR/src/correlation.c
+++ benchmarks/Polybench/CORR/src/correlation.c
@@ -20,18 +20,6 @@
 
 #include "BenchmarksUtil.h"
 
-// define the error threshold for the results "not matching"
-#define ERROR_THRESHOLD 1.05
-
-/* Problem size. */
-#ifdef RUN_TEST
-#define SIZE 1100
-#elif RUN_BENCHMARK
-#define SIZE 9600
-#else
-#define SIZE 1000
-#endif
-
 /* Problem size */
 #define M SIZE
 #define N SIZE
@@ -44,6 +32,11 @@
 /* Can switch DATA_TYPE between float and double */
 typedef float DATA_TYPE;
 
+/**
+ * @brief Initialize matrice
+ * 
+ * @param data 
+ */
 void init_arrays(DATA_TYPE *data) {
   int i, j;
 
@@ -54,6 +47,39 @@ void init_arrays(DATA_TYPE *data) {
   }
 }
 
+/**
+ * @brief 
+ * 
+ * @param symmat 
+ * @param symmat_outputFromGpu 
+ * @return int 
+ */
+int compareResults(DATA_TYPE *symmat, DATA_TYPE *symmat_outputFromGpu) {
+  int i, j, fail;
+  fail = 0;
+
+  for (i = 1; i < (M + 1); i++) {
+    for (j = 1; j < (N + 1); j++) {
+      if (percentDiff(symmat[i * (N + 1) + j],
+                      symmat_outputFromGpu[i * (N + 1) + j]) >
+          ERROR_THRESHOLD) {
+        fail++;
+      }
+    }
+  }
+
+  return fail;
+}
+
+
+/**
+ * @brief 
+ * 
+ * @param data 
+ * @param mean 
+ * @param stddev 
+ * @param symmat 
+ */
 void correlation(DATA_TYPE *data, DATA_TYPE *mean, DATA_TYPE *stddev,
                  DATA_TYPE *symmat) {
   int i, j, j1, j2;
@@ -111,13 +137,21 @@ void correlation(DATA_TYPE *data, DATA_TYPE *mean, DATA_TYPE *stddev,
   symmat[M * (M + 1) + M] = 1.0;
 }
 
+/**
+ * @brief 
+ * 
+ * @param data 
+ * @param mean 
+ * @param stddev 
+ * @param symmat 
+ */
 void correlation_OMP(DATA_TYPE *data, DATA_TYPE *mean, DATA_TYPE *stddev,
                      DATA_TYPE *symmat) {
   int i, j, k;
-  #pragma omp target data map(to: data[:(M+1)*(N+1)], mean[:(M+1)], stddev[:(M+1)]) map(tofrom: symmat[:(M+1)*(N+1)]) device(DEVICE_ID)
+  #pragma omp target data map(to: data[:(M+1)*(N+1)], mean[:(M+1)], stddev[:(M+1)]) map(tofrom: symmat[:(M+1)*(N+1)]) device(OMP_DEVICE_ID)
   {
     // Determine mean of column vectors of input data matrix
-    #pragma omp target teams distribute  parallel for private(i) device(DEVICE_ID)
+    #pragma omp target teams distribute  parallel for private(i) device(OMP_DEVICE_ID)
     for (j = 1; j < (M + 1); j++) {
       mean[j] = 0.0;
       for (i = 1; i < (N + 1); i++) {
@@ -127,7 +161,7 @@ void correlation_OMP(DATA_TYPE *data, DATA_TYPE *mean, DATA_TYPE *stddev,
     }
 
     // Determine standard deviations of column vectors of data matrix.
-    #pragma omp target teams distribute parallel for private(i) device(DEVICE_ID)
+    #pragma omp target teams distribute parallel for private(i) device(OMP_DEVICE_ID)
     for (j = 1; j < (M + 1); j++) {
       stddev[j] = 0.0;
       for (i = 1; i < (N + 1); i++) {
@@ -143,7 +177,7 @@ void correlation_OMP(DATA_TYPE *data, DATA_TYPE *mean, DATA_TYPE *stddev,
     }
 
     // Center and reduce the column vectors.
-    #pragma omp target teams distribute parallel for collapse(2) device(DEVICE_ID)
+    #pragma omp target teams distribute parallel for collapse(2) device(OMP_DEVICE_ID)
     for (i = 1; i < (N + 1); i++) {
       for (j = 1; j < (M + 1); j++) {
         data[i * (M + 1) + j] -= mean[j];
@@ -152,7 +186,7 @@ void correlation_OMP(DATA_TYPE *data, DATA_TYPE *mean, DATA_TYPE *stddev,
     }
 
     // Calculate the m * m correlation matrix.
-    #pragma omp target teams distribute parallel for private(j, i) device(DEVICE_ID)
+    #pragma omp target teams distribute parallel for private(j, i) device(OMP_DEVICE_ID)
     for (k = 1; k < M; k++) {
       symmat[k * (M + 1) + k] = 1.0;
       for (j = k + 1; j < (M + 1); j++) {
@@ -169,73 +203,44 @@ void correlation_OMP(DATA_TYPE *data, DATA_TYPE *mean, DATA_TYPE *stddev,
   symmat[M * (M + 1) + M] = 1.0;
 }
 
-int compareResults(DATA_TYPE *symmat, DATA_TYPE *symmat_outputFromGpu) {
-  int i, j, fail;
-  fail = 0;
-
-  for (i = 1; i < (M + 1); i++) {
-    for (j = 1; j < (N + 1); j++) {
-      if (percentDiff(symmat[i * (N + 1) + j],
-                      symmat_outputFromGpu[i * (N + 1) + j]) >
-          ERROR_THRESHOLD) {
-        fail++;
-        // printf("i: %d j: %d\n1: %f 2: %f\n", i, j, symmat[i*N + j],
-        // symmat_GPU[i*N + j]);
-      }
-    }
-  }
-
-  // print results
-  printf("Non-Matching CPU-GPU Outputs Beyond Error Threshold of %4.2f "
-         "Percent: %d\n",
-         ERROR_THRESHOLD, fail);
-
-  return fail;
-}
-
 int main() {
-  double t_start, t_end;
-  int fail = 0;
-
-  DATA_TYPE *data;
-  DATA_TYPE *mean;
-  DATA_TYPE *stddev;
-  DATA_TYPE *symmat;
-  DATA_TYPE *symmat_GPU;
-
-  data = (DATA_TYPE *)malloc((M + 1) * (N + 1) * sizeof(DATA_TYPE));
-  mean = (DATA_TYPE *)malloc((M + 1) * sizeof(DATA_TYPE));
-  stddev = (DATA_TYPE *)malloc((M + 1) * sizeof(DATA_TYPE));
-  symmat = (DATA_TYPE *)malloc((M + 1) * (N + 1) * sizeof(DATA_TYPE));
-  symmat_GPU = (DATA_TYPE *)malloc((M + 1) * (N + 1) * sizeof(DATA_TYPE));
-
   fprintf(stdout, "<< Correlation Computation >>\n");
 
+  // declare arrays and allocate memory
+  DATA_TYPE *data = (DATA_TYPE *) malloc((M + 1) * (N + 1) * sizeof(DATA_TYPE));
+  DATA_TYPE *mean = (DATA_TYPE *) malloc((M + 1) * sizeof(DATA_TYPE));
+  DATA_TYPE *stddev = (DATA_TYPE *) malloc((M + 1) * sizeof(DATA_TYPE));
+  DATA_TYPE *symmat = NULL;
+  DATA_TYPE *symmat_GPU = NULL;
+
+  // init operand matrices
   init_arrays(data);
 
-  t_start = rtclock();
-  correlation_OMP(data, mean, stddev, symmat_GPU);
-  t_end = rtclock();
-
-  fprintf(stdout, "GPU Runtime: %0.6lfs\n", t_end - t_start);
-
-#ifdef RUN_TEST
-  init_arrays(data);
-
-  t_start = rtclock();
-  correlation(data, mean, stddev, symmat);
-  t_end = rtclock();
-
-  fprintf(stdout, "CPU Runtime: %0.6lfs\n", t_end - t_start);
-
-  compareResults(symmat, symmat_GPU);
+// run OMP on GPU or CPU if enabled
+#if defined(RUN_OMP_GPU) || defined(RUN_OMP_CPU)
+  symmat_GPU = (DATA_TYPE *) malloc((M + 1) * (N + 1) * sizeof(DATA_TYPE));
+  BENCHMARK_OMP(correlation_OMP(data, mean, stddev, symmat_GPU));
 #endif
 
+// run sequential version if enabled
+#ifdef RUN_CPU_SEQ
+  symmat = (DATA_TYPE *) malloc((M + 1) * (N + 1) * sizeof(DATA_TYPE));
+  BENCHMARK_CPU(correlation(data, mean, stddev, symmat));
+#endif
+
+  int fail = 0;
+// if TEST is enabled, then compare OMP results against sequential mode
+#ifdef RUN_TEST
+  fail = compareResults(symmat, symmat_GPU);
+  printf("Errors on OMP (threshold %4.2lf): %d\n", ERROR_THRESHOLD, fail);
+#endif
+
+  // release memory
   free(data);
   free(mean);
   free(stddev);
-  free(symmat);
   free(symmat_GPU);
+  free(symmat);
 
   return fail;
 }
